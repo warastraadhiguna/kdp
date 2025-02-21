413c33eb502d440861aef8984b717e86fa267e57
anglingkusumandhita <anglingkusumandhita@gmail.com>
2025-02-21 09:17:50 +0700

    change password

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers\Filament;
 
 use App\Filament\Pages\DashboardCustom;
+use App\Filament\Pages\Profile;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -35,6 +36,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 DashboardCustom::class,
+                Profile::class,
             ])
             ->spa()
             ->sidebarCollapsibleOnDesktop()
