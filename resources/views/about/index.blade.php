fd50bf895ed1872feb246fc4f383a8137f11970f
anglingkusumandhita <anglingkusumandhita@gmail.com>
2025-06-26 11:12:04 +0700

    new layout about

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,32 +1,6 @@
 
 @extends('layouts.index')
 @section('content')
- <!-- Start Common BreadCrumb -->
-  {{-- <section>
-    <div class="cs-breadcrumb-wrap theme-dark" data-src="{{ asset('storage/' . $company->breadcrumb_image) }}">
-      <div class="container">
-        <div class="row cs_center">
-          <div class="cs-bread-page-title-area">
-            <div class="cs-page-title">
-              <h2 class="cs_white_color">About Us</h2>
-            </div>
-            <div class="breadcrumb">
-              <ul>
-                <li>
-                  <a href="{{ url('/')}}" class="cs-text_b_line"><span>HOME</span></a>
-                </li>
-                <li>/</li>
-                <li>ABOUT US</li>
-              </ul>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  </section> --}}
-  <!-- End Common BreadCrumb -->
-
   <section>
     <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-inner">
@@ -45,58 +19,62 @@
     </div>
   </section>
 
- <!-- Start Company Journey -->
-  <section>
-    <div class="cs-cj-wrap">
-      <div class="cj-left">
-        <div class="cs-section-height"></div>
-        <div class="cj-left-content">
-          <h2 data-aos="fade-up" data-aos-duration="500">About Us</h2>
-          <div class="cs-height-25"></div>
-          <div class="max-width-600">
-            <p  data-aos="fade-up" data-aos-duration="700"  style="white-space: pre-line;text-align:justify; margin-right: 10px;">{{ $company->about_us }}</p>
-            <div class="cs-height-30"></div>
-          </div>
-            <div class="cs-height-165"></div>
-        </div>
-        <img src="{{ asset('storage/' . $company->about_us_image) }}" alt="">
+  <section class="container py-5">
+    <div class="row">
+      <div class="col-md-6">
+        <p style="text-align: justify;"  data-aos="fade-up" data-aos-duration="700"  style="white-space: pre-line;text-align:justify; margin-right: 10px;">
+          {{ $company->about_us }}
+        </p> 
       </div>
-      <div class="cj-right" >
 
-        <div class="cs-height-70"></div>        
-        <div class="cs-section-height"></div>
-        <div class="cs-height-40"></div>
-
-        <div class="timeline-pagination"  style="margin-left: -40px"></div>
-        <div class="timeline-progress"  style="margin-left: -1px">
-          <div class="timeline-progress-bar"></div>
-        </div>
-
-        <div class="swiper timeline-slider">
-          <div class="swiper-wrapper">
-            @foreach($briefHistories as $briefHistory)
-                          <div class="swiper-slide">
-              <div class="cs-height-50"></div>
-              <img src="{{ asset('storage/' . $briefHistory->image) }}" alt=" {{ $briefHistory->year }}">
-              <div class="cj-right-content">
-
-                <div class="cs-height-40"></div>
-                <div class="cs-heading-with-animation text-left m-auto d-flex">
-                  <h2 class="cs-heading">{{ $briefHistory->year }}</h2>
-                  <span class="cs-text-style-h1 cs-animated-text">INITIAL START {{ $briefHistory->year }}</span>
-                </div>
-                <p  style="white-space: pre-line;text-align:justify">{{ $briefHistory->note }}
-                </p>
-              </div>
-            </div>
-            @endforeach
-
-          </div>
-        </div>
-        
+      <div class="col-md-6">
+        <img src="{{ asset('storage/' . $company->about_us_image) }}" alt="About Us Image" class="img-fluid rounded">
       </div>
     </div>
   </section>
+  
+  <section class="container">
+    <div class="row">
+      <div class="col text-center my-5">        
+        <h2>Company History</h2>
+      </div>
+    </div>    
+    <div class="row">
+      <div class="col my-5">
+      <div class="timeline-pagination" style="margin-left: -40px"></div>
+        <div class="timeline-progress" style="margin-left: -1px">
+          <div class="timeline-progress-bar"></div>
+        </div>
+        <div class="cs-height-40"></div>
+        <div class="swiper timeline-slider">
+          <div class="swiper-wrapper">
+            @foreach($briefHistories as $briefHistory)
+              <div class="swiper-slide">
+                <div class="d-flex align-items-center">
+                  <!-- Gambar di kiri -->
+                  <div class="swiper-slide-image col-md-6">
+                    <img src="{{ asset('storage/' . $briefHistory->image) }}" alt=" {{ $briefHistory->year }}" class="img-fluid rounded">
+                  </div>
+  
+                  <!-- Teks di kanan -->
+                  <div class="swiper-slide-text col-md-6">
+                    <div class="cs-height-50"></div>
+                    <div class="cs-heading-with-animation text-left m-auto d-flex">
+                      <h2 class="cs-heading">{{ $briefHistory->year }}</h2>
+                      <span class="cs-text-style-h1 cs-animated-text">INITIAL START {{ $briefHistory->year }}</span>
+                    </div>
+                    <p style="white-space: pre-line; text-align:justify">{{ $briefHistory->note }}</p>
+                  </div>
+                </div>
+              </div>
+            @endforeach
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+
   <!-- End Company Journey -->  
   @include('layouts.tagline');      
 @endsection
